621c6e7a0aca1bc1a2254bfd47ec469e7fb297e2
Jakub Pyzio <jakub@pyzio.eu>
2019-04-07 22:52:12 +0200

    refactor

--- installer/ssh-keygen.sh
+++ installer/ssh-keygen.sh
@@ -2,14 +2,17 @@
 
 progress "SSH Key"
 
-PASSPHRASE=`zenity --password --title="SSH" --text="Enter your RSA key passphrase"`
+SSH_DIR=${HOME}/.ssh/
+PRIVATE_KEY_PATH=${SSH_DIR}id_rsa
+PUBLIC_KEY=`cat ${SSH_DIR}id_rsa.pub`
 
-if [[ -f ${HOME}/.ssh/id_rsa ]]; then
+if [[ -f ${PRIVATE_KEY_PATH} ]]; then
     zenity --info --title="SSH" --text="Your RSA key is already generated. Remove it if you want to generate new key." --width=200 --height=50
 else
-    mkdir "${HOME}/.ssh"
-    ssh-keygen -q -t rsa -N "${PASSPHRASE}" -o -a 100 -b 4096 -f "${HOME}/.ssh/id_rsa"
+    mkdir -p ${SSH_DIR}
+
+    PASSPHRASE=`zenity --password --title="SSH" --text="Enter your RSA key passphrase"`
+    ssh-keygen -q -t rsa -N "${PASSPHRASE}" -o -a 100 -b 4096 -f "${SSH_DIR}id_rsa"
 fi
 
-PUBLIC_KEY=`cat ~/.ssh/id_rsa.pub`
 zenity --entry --title="SSH" --text="This is your Public key, paste it into Git repositories" --entry-text="${PUBLIC_KEY}"
