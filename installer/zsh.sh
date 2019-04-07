461d66e67789d838858f8857c7dbb2b506f97ca2
Jakub Pyzio <jakub@pyzio.eu>
2019-04-07 20:13:15 +0200

    fzf for zsh

--- installer/zsh.sh
+++ installer/zsh.sh
@@ -6,4 +6,7 @@ sudo apt install -y zsh gawk
 
 message "PRESS [ENTER], after oh-my-zsh installation, press CTRL+D to resume installation"
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
