5d5f762c0c41cc782860a8c0b6c20382d194fbc6
Jakub Pyzio <jakub@pyzio.eu>
2019-04-07 22:58:19 +0200

    refactor

--- installer/git-hooks.sh
+++ installer/git-hooks.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-TEMPLATES_PATH="${HOME}/.git-templates/"
-HOOKS_PATH="${ROOT_DIR}/git_hooks/"
+HOOKS_DIR="${ROOT_DIR}/git_hooks/"
+USER_GIT_TEMPLATES_DIR="${HOME}/.git-templates/"
+USER_GIT_HOOKS_DIR=${USER_GIT_TEMPLATES_DIR}hooks/
 
-git config --global init.templatedir ${TEMPLATES_PATH}
-mkdir -p ${TEMPLATES_PATH}hooks
-cp ${HOOKS_PATH}* ${TEMPLATES_PATH}hooks/
-chmod +x ${TEMPLATES_PATH}hooks/*
+git config --global init.templatedir ${USER_GIT_TEMPLATES_DIR}
+
+mkdir -p ${USER_GIT_HOOKS_DIR}
+
+cp ${HOOKS_DIR}* ${USER_GIT_HOOKS_DIR}
+
+chmod +x ${USER_GIT_HOOKS_DIR}*
