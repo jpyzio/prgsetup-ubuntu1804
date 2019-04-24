62ae9c13a3533618a15c7f9982d0d896a58e1b07
Jakub Pyzio <jakub@pyzio.eu>
2019-04-24 21:46:20 +0200

    virtualbox: fix repo arch

--- modules/virtualbox.sh
+++ modules/virtualbox.sh
@@ -3,7 +3,7 @@
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib"
 
 sudo apt update
 sudo apt install -y virtualbox virtualbox-ext-pack
