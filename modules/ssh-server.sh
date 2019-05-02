fdf6bc883cc806cc153791177ed368d574897e27
Jakub Pyzio <jakub@pyzio.eu>
2019-05-02 02:12:11 +0200

    ssh server security

--- modules/ssh-server.sh
+++ modules/ssh-server.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 sudo apt install -y openssh-server
+
+sudo sed -i -E "s/.*PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*PermitEmptyPasswords.*/PermitEmptyPasswords no/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*ClientAliveInterval.*/ClientAliveInterval 600/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*ClientAliveCountMax.*/ClientAliveCountMax 0/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*X11Forwarding.*/X11Forwarding no/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*Banner.*/Banner \/etc\/issue/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*IgnoreRhosts.*/IgnoreRhosts yes/g" /etc/ssh/sshd_config
+sudo sed -i -E "s/.*HostbasedAuthentication.*/HostbasedAuthentication no/g" /etc/ssh/sshd_config
+
+sudo sudo adduser ${user} sudo
+
+sudo service ssh restart
+
+
+#Install fail2ban for openssh server
