d11ff4acf4ea349d0d8c4c44458c6734f40b7bd2
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 23:52:54 +0200

    Smartmontools without recommends

--- modules/system-tools.sh
+++ modules/system-tools.sh
@@ -4,7 +4,8 @@ sudo apt update
 
 
 ### BEGIN Misc system tools
-sudo apt install --yes sshfs curlftpfs gparted smartmontools secure-delete
+sudo apt install --yes sshfs curlftpfs gparted secure-delete
+sudo apt install --yes smartmontools --no-install-recommends
 ### END Misc system tools
 
 
