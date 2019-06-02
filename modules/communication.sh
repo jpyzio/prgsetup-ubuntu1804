47f1819aca7484571cdb1d764bc2eecf6492fff7
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 05:01:44 +0200

    Refactor communication

--- modules/communication.sh
+++ modules/communication.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt update
+
+
+### BEGIN Slack
 SLACK_FILE="${ROOT_DIR}/slack.deb"
 
 wget --output-document=${SLACK_FILE} https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
@@ -7,3 +11,9 @@ wget --output-document=${SLACK_FILE} https://downloads.slack-edge.com/linux_rele
 sudo gdebi -n ${SLACK_FILE}
 
 rm ${SLACK_FILE}
+### END Slack
+
+
+### BEGIN Thunderbird
+sudo apt install --yes thunderbird
+### END Thunderbird
