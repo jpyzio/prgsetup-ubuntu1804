ee7f222b35d0477a570fbcd8d72af80f10e576f7
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 23:24:08 +0200

    Fix installation menu

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 CONFIGURATOR_VERSION="18.04"
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 MODULES_DIR="${ROOT_DIR}/modules"
-CONFIG_FILE="${ROOT_DIR}/config.sh"
+CONFIG_FILE="${ROOT_DIR}/config.local.sh"
 
 input() {
     zenity --entry --title="Ubuntu Configurator" --text="${1}"
@@ -21,34 +21,35 @@ sudo echo -e "\e[32mLet's start the installation ;)\e[39m";
 
 CHOICES=$(whiptail --checklist "Select which services do you want install. " \
     20 55 15 \
-    "diagnostic" "Diagnostic tools" on \
-    "zsh" "Z Shell" off \
-    "google-chrome" "Google Chrome" on \
-    "gpg" "GPG Key" off \
-    "git" "Git" on \
-    "git-hooks" "Hooks for Git" on \
-    "jetbrains-toolbox" "Jetbrains Toolbox" on \
-    "docker" "Docker & Docker Compose" on  \
-    "ssh-keygen" "Generate RSA key" on \
-    "slack" "Slack communicator" on \
-    "office" "Office apps" on \
-    "ufw" "Enable firewall" on \
-    "vpn-client" "VPN Client" on \
-    "php" "PHP 7.2 with extensions" off  \
-    "frontend-tools" "Node.js + Yarn + Vue" off \
-    "spotify" "Spotify" off \
-    "ssh-server" "Open SSH Server" off \
-    "atom" "Atom editor" off \
-    "virtualbox" "VirtualBox" off \
-    "rest-tools" "REST Tools" off \
-    "sublime3" "Sublime Text 3" off \
+    "update-system" "Update system" on \
+    "system-tools" "System tools" on \
+    "diagnostic-tools" "Diagnostic tools" on \
+    "dev-tools-backend" "Tools for backend developers" on \
+    "dev-tools-common" "Tools for all developers" on \
+    "dev-tools-frontend" "Tools for frontend developers" on \
+    "terminal" "Eg. Z Shell and other modifications" on \
+    "browsers" "Eg. Chrome, Firefox" on \
+    "gpg" "Keys" off \
+    "git" "with custom hooks and configs" on \
+    "ide-editors" "Eg. Sublime Text 3, Atom, Jetbrains Toolbox" on \
+    "docker" "With docker-compose" on \
+    "ssh-keygen" "Generate SSH Key" on \
+    "communication" "Eg. Slack" on \
+    "office" "Eg. Libre Office, GIMP" on \
+    "security" "Eg. Firewall" on \
+    "php" "7.2 with composer" on \
+    "media" "Eg. Spotify, VLC" on \
+    "ssh-server" "With secure configuration" off \
+    "virtualbox" "If you want install other systems ;)" off \
+    "documentation" "Generators, converters etc" off \
+
     3>&2 2>&1 1>&3 )
 
 if [[ -f ${CONFIG_FILE} ]]; then
     source ${CONFIG_FILE}
 fi
 
-source ${MODULES_DIR}/misc.sh
+source ${MODULES_DIR}/required.sh
 
 for CHOICE in ${CHOICES}; do
     source "${MODULES_DIR}/`echo ${CHOICE} | tr -d '"'`.sh"
