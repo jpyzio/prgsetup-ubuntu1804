8ceffbebbd5d32433809d9a56d8abd2d3e444ec6
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 18:57:09 +0200

    Create backend tools

---
+++ modules/dev-tools-backend.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+### BEGIN Redis Desktop Manager
+sudo snap install redis-desktop-manager
+### END Redis Desktop Manager
+
+
+### BEGIN Insomnia
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee /etc/apt/sources.list.d/insomnia.list
+
+sudo apt-get update
+sudo apt-get install --yes insomnia
+### END Insomnia
+
+### BEGIN Postman
+sudo snap install postman
+### END Postman
+
+
+### BEGIN Composer global tools
+composer global require phpunit/phpunit
+composer global require phing/phing
+composer global require sebastian/phpcpd
+composer global require phploc/phploc
+composer global require phpmd/phpmd
+composer global require squizlabs/php_codesniffer
+### END Composer global tools
