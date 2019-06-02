eacba922f7779db808d5288d9c6ef276f3e7ed25
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 15:31:19 +0200

    Refactor gpg file

--- modules/gpg.sh
+++ modules/gpg.sh
@@ -2,28 +2,39 @@
 
 KEYGEN_CONFIG_FILE="${ROOT_DIR}/keygen_config"
 
-sudo apt install -y gpa gpg
 
-while [[ -z ${GPG_REAL_NAME} ]] ; do
-    GPG_REAL_NAME=`input "GPG - Real name"`
+### BEGIN Update and install dependencies
+sudo apt update
+sudo apt install --yes shred
+### END Update and install dependencies
+
+
+### BEGIN GPG and tools
+sudo apt install --yes gpa gpg
+### END GPG and tools
+
+
+### BEGIN GPG configuration
+while [[ -z ${REAL_NAME} ]] ; do
+    REAL_NAME=`input "GPG - Real name"`
 done
 
 while [[ -z ${GPG_EMAIL} ]] ; do
-    GPG_EMAIL=`input "GPG - Email"`
+    EMAIL=`input "GPG - Email"`
 done
 
-while [[ -z ${GPG_PASSPHRASE} ]] ; do
-    GPG_PASSPHRASE=`input "Git Global - Your name"`
+while [[ -z ${PASSPHRASE} ]] ; do
+    PASSPHRASE=`input "Git Global - Your name"`
 done
 
 echo "Key-Type: 1
 Key-Length: 4096
 Subkey-Type: 1
 Subkey-Length: 4096
-Name-Real: ${GPG_REAL_NAME}
-Name-Email: ${GPG_EMAIL}
+Name-Real: ${REAL_NAME}
+Name-Email: ${EMAIL}
 Expire-Date: 0
-Passphrase: ${GPG_PASSPHRASE}
+Passphrase: ${PASSPHRASE}
 " > ${KEYGEN_CONFIG_FILE}
 
 gpg --gen-key --batch ${KEYGEN_CONFIG_FILE}
@@ -32,6 +43,10 @@ shred --remove --iterations=100 ${KEYGEN_CONFIG_FILE}
 
 GPG_ID=`gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5`
 if [[ ! -z "${GPG_ID}" ]]; then
+    sed -i -E "s/.*export GPGKEY.*\n//g" ~/.bashrc
+    sed -i -E "s/.*export GPGKEY.*\n//g" ~/.zshrc
+
     echo "export GPGKEY=${GPG_ID}" >> ~/.bashrc
     echo "export GPGKEY=${GPG_ID}" >> ~/.zshrc
 fi
+### END GPG configuration
