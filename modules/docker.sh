27df7d92cf654dfa711e533d17cc084834b935f3
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 02:30:30 +0200

    Fix docker installer

--- modules/docker.sh
+++ modules/docker.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-sudo apt purge -y docker docker-engine docker.io containerd runc
-
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+### BEGIN Docker
+sudo apt purge --yes docker docker-engine docker.io containerd runc
+sudo apt install --yes apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl --fail --silent --show-error --location https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-sudo docker run hello-world
-
-sudo usermod -aG docker ${USER}
-
-sudo docker run hello-world
-
-sudo systemctl enable docker
+sudo apt install --yes docker-ce docker-ce-cli containerd.io
 
 echo '{"dns": ["8.8.8.8", "8.8.4.4"]}' | sudo tee /etc/docker/daemon.json
+sudo systemctl enable docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo usermod --append --groups docker ${USER}
+
+sudo docker run hello-world
+### END Docker
+
+
+### BEGIN Docker Compose
+sudo curl --location "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname --kernel-name)-$(uname --machine)" --output /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln --symbolic /usr/local/bin/docker-compose /usr/bin/docker-compose
+### END Docker Compose
 
-echo -e "\e[31mWARNING!!! Before using the docker commands, restart the system!\e[39m"
+echo -e "\e[31mWARNING!!! Before you use the docker command without \"sudo\", please restart the system!\e[39m"
