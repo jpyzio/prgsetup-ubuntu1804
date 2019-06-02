fb3a97ab6906759587bc61b955a1e45f776dc5aa
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 04:57:33 +0200

    Refactor VirtualBox

--- modules/virtualbox.sh
+++ modules/virtualbox.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt update
+
+
+### BEGIN VirtualBox
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib"
 
 sudo apt update
-sudo apt install -y virtualbox virtualbox-ext-pack
+sudo apt install --yes virtualbox virtualbox-ext-pack
+### END VirtualBox
