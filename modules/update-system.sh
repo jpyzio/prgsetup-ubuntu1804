28ab9f6bada5f0d2dec5997d1f8bb6e34c4ddbce
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 21:41:34 +0200

    Add update system module

---
+++ modules/update-system.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
+sudo apt full-upgrade --yes
+
+sudo apt autoremove --yes
+
+sudo apt autoclean --yes
+
+if [[ command -v yarn > /dev/null ]]; then
+    yarn global upgrade
+fi
+
+if [[ command -v composer > /dev/null ]]; then
+    sudo composer self-update
+    composer global update
+fi
