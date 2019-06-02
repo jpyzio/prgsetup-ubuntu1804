02d5cc273ee95fd082b16860787336c75cf255ce
Jakub Pyzio <jakub@pyzio.eu>
2019-06-03 00:33:12 +0200

    Fix 7zip package

--- modules/dev-tools-common.sh
+++ modules/dev-tools-common.sh
@@ -8,5 +8,5 @@ sudo apt install --yes filezilla rsync
 ### END File transfer apps
 
 ### BEGIN Compression apps
-sudo apt install --yes 7-zip
+sudo apt install --yes p7zip-full
 ### END Compression apps
