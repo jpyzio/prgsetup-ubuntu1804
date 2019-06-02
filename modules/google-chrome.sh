92b5cb90771f20f1ba222d2bbdf58ef1bcffb65f
Jakub Pyzio <jakub@pyzio.eu>
2019-06-02 02:25:40 +0200

    Cleanup before refactor

--- modules/google-chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CHROME_FILE="${ROOT_DIR}/google-chrome.deb"
-
-wget --output-document=${CHROME_FILE} https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-sudo gdebi -n ${CHROME_FILE}
-
-rm ${CHROME_FILE}
