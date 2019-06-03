b325d5ee7f2e9e22b98bde9f042a1c2a14b83c48
Jakub Pyzio <jakub@pyzio.eu>
2019-06-03 11:46:51 +0200

    Fix sublime-text 3

--- modules/ide-editors.sh
+++ modules/ide-editors.sh
@@ -14,10 +14,7 @@ rm ${ATOM_FILE}
 
 
 ### END Sublime Text 3
-wget --quiet -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-sudo apt install --yes sublime-text
+sudo snap install sublime-text
 ### END Sublime Text 3
 
 
