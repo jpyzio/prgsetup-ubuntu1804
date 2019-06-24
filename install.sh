340d0f4f10e15de5c79f73ad59efe80fb57116b9
Jakub Pyzio <jakub@pyzio.eu>
2019-06-25 01:36:03 +0200

    Revert install.sh

--- install.sh
+++ install.sh
@@ -24,9 +24,6 @@ sudo echo -e "\e[32mLet's start the installation ;)\e[39m";
 CHOICES=$(whiptail --checklist "Select which services do you want install. " \
     30 77 22 \
     "update-system" "Update system" on \
-    "gpg" "GNU Privacy Guard Keys" off \
-    "git" "with custom hooks and configs" on \
-    "ssh-keygen" "Generate SSH Key" on \
     "system-tools" "System tools" on \
     "diagnostic-tools" "Diagnostic tools" on \
     "dev-tools-backend" "Tools for backend developers" on \
@@ -34,16 +31,19 @@ CHOICES=$(whiptail --checklist "Select which services do you want install. " \
     "dev-tools-frontend" "Tools for frontend developers" on \
     "terminal" "Eg. Z Shell and other modifications" on \
     "browsers" "Eg. Chrome, Firefox" on \
+    "gpg" "GNU Privacy Guard Keys" off \
+    "git" "with custom hooks and configs" on \
     "ide-editors" "Eg. Sublime Text 3, Atom, Jetbrains Toolbox" on \
     "docker" "With docker-compose" on \
+    "ssh-keygen" "Generate SSH Key" on \
     "communication" "Eg. Slack" on \
     "office" "Eg. Libre Office, GIMP" on \
     "security" "Eg. Firewall" on \
     "media" "Eg. Spotify, VLC" on \
     "ssh-server" "With secure configuration" off \
+    "virtualbox" "If you want install other systems ;)" off \
     "documentation" "Generators, converters etc" off \
     "jacobs-custom" "Jacob's customization" off \
-    "virtualbox" "If you want install other systems ;)" off \
     3>&2 2>&1 1>&3 )
 
 source ${MODULES_DIR}/required.sh
