025665b132da127af2dbaae763a90a8452a194c7
Jakub Pyzio <jakub@pyzio.eu>
2019-06-24 12:23:32 +0200

    Install rkhunter without postfix

--- modules/security.sh
+++ modules/security.sh
@@ -2,7 +2,8 @@
 
 
 ### BEGIN Security tools
-sudo apt install --yes  ufw rkhunter
+sudo apt install --yes ufw
+sudo apt install --yes --no-install-recommends rkhunter
 
 sudo ufw enable
 ### END Security tools
