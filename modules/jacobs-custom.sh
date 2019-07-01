168149636a0ec8eff3620a63f3f4eb7bfa3f6cae
Jakub Pyzio <jakub@pyzio.eu>
2019-07-01 13:16:16 +0200

    Useless swap

--- modules/jacobs-custom.sh
+++ modules/jacobs-custom.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-### BEGIN SWAP
-echo "vm.swappiness = 10" | sudo tee --append /etc/sysctl.conf
-
-sudo fallocate -l 1G /swapfile
-
-sudo chmod 600 /swapfile
-
-sudo mkswap /swapfile
-
-echo '#/swapfile    swap    swap    defaults    0   0' | sudo tee --append /etc/fstab
-### END SWAP
-
 
 ### BEGIN DOCK
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
