fa418eb9e8f5d6680a860992c2ddb64f7418f345
Jakub Pyzio <jakub@pyzio.eu>
2019-07-05 02:35:06 +0200

    Fix shellcheck errors #14

--- modules/browsers.sh
+++ modules/browsers.sh
@@ -4,11 +4,11 @@
 ### BEGIN Google Chrome
 CHROME_FILE="${ROOT_DIR}/google-chrome.deb"
 
-wget --output-document=${CHROME_FILE} https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget --output-document="${CHROME_FILE}" https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo gdebi --non-interactive ${CHROME_FILE}
+sudo gdebi --non-interactive "${CHROME_FILE}"
 
-rm ${CHROME_FILE}
+rm "${CHROME_FILE}"
 ### END Google Chrome
 
 
