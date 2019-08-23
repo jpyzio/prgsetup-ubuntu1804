aee139a2317df68519eab49742e1044d9fd2131f
Jakub Pyzio <jakub@pyzio.eu>
2019-08-23 11:45:54 +0200

    Install VLC and Spotify with Snap

--- modules/media.sh
+++ modules/media.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-### BEGIN Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-sudo apt update
-sudo apt install --yes spotify-client
-### END Spotify
-
-### BEGIN VLC
-sudo apt install --yes vlc
-### END VLC
+### BEGIN Media
+snap install vlc spotify
+### END Media
