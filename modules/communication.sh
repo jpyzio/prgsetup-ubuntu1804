f930bd762a0c4cba4519b560a910cc1db9655f59
Ovvl <jakub@ovvl.it>
2019-12-07 18:07:04 +0100

    Slack update 3.3.8 => 4.2.0

--- modules/communication.sh
+++ modules/communication.sh
@@ -3,7 +3,7 @@
 ### BEGIN Slack
 SLACK_FILE="${ROOT_DIR}/slack.deb"
 
-wget --output-document="${SLACK_FILE}" https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
+wget --output-document="${SLACK_FILE}" https://downloads.slack-edge.com/linux_releases/slack-desktop-4.2.0-amd64.deb
 
 sudo gdebi --non-interactive "${SLACK_FILE}"
 
